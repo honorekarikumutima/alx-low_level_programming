13d6865fd38613b937eb74c1a80e63144c870a61
honorekarikumutima <karikumutima_220001299@stud.ur.ac.rw>
2023-03-08 07:46:38 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followes by a new line.
- * @s: The streng to be printed.
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s)
@@ -12,7 +11,7 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	
+
 	else
 		_putchar('\n');
 }

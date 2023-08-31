bcd68e50b66802552fcbc5b45bf171376eab69bc
honorekarikumutima <honorekarikumutima@gmail.com>
2023-08-31 07:28:23 -0700

    Task 0 Binary unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,34 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
- */
+ * binary_to_uint - covert from binary to decimal
+ * @b: binary value to be converted
+ * Return: Allow success
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int sum = 0;
+	unsigned int len_b = 0, dec_val = 0, i = 0, tmp_b, num;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-
-	for (i = 0; b[i]; i++)
+	while (b[i])
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
-		sum = 2 * sum + (b[i] - '0');
+		len_b++;
+		i++;
 	}
-
-	return (sum);
+	if (len_b == 0)
+		return (0);
+	len_b = len_b - 1;
+	tmp_b = len_b;
+	for (i = 0; i <= len_b; i++)
+	{
+		if (b[i] == '0')
+			num = 0;
+		else if (b[i] == '1')
+			num = 1;
+		dec_val = dec_val + (num << tmp_b);
+		tmp_b--;
+	}
+	return (dec_val);
 }
-
